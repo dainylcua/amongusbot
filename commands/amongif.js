092d3c7def0148fc3c3af8b8ac0b7f400a20f129
dainylcua <dainylcua@gmail.com>
2021-12-03 22:57:03 -0600

    Tenor gif support over GIPHY

--- commands/amongif.js
+++ commands/amongif.js
@@ -2,16 +2,16 @@
 const { SlashCommandBuilder } = require('@discordjs/builders')
 const fetch = require('node-fetch')
 
-const GIPHY_URL = 'http://api.giphy.com/v1/gifs/search'
+const TENOR_URL = 'https://g.tenor.com/v1/search'
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('amongif')
     .setDescription('Replies with an amongus gif'),
   async execute(interaction) {
-    const offset = Math.floor(Math.random() * (15 + 1))
-    const res = await fetch(`${GIPHY_URL}?api_key=${process.env.GIPHY_KEY}&q=amogus&limit=1&offset=${offset}`)
+    const pos = Math.floor(Math.random() * (15 + 1))
+    const res = await fetch(`${TENOR_URL}?api_key=${process.env.TENOR_KEY}&q=amogus&limit=1&pos=${pos}`)
     const data = await res.json()
-    await interaction.reply(data.data[0].images.downsized.url)
+    await interaction.reply(data.results[0].media[0].gif.url)
   }
 }
